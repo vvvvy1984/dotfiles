3e257a73b20755646ffee51d82c6422735b85d05
Cosmin Stejerean <cstejerean@gmail.com>
2008-09-15 21:25:26 -0500

    renamed bash stuff to remove bash- prefix since it's in bash subfolder now

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ if [[ -z $SHARED_PROFILE_HOME ]] ; then
     export SHARED_PROFILE_HOME=~/checkouts/shared-profile
 fi
 
-source $SHARED_PROFILE_HOME/bash/bash-env
-source $SHARED_PROFILE_HOME/bash/bash-aliases
-source $SHARED_PROFILE_HOME/bash/bash-functions
+source $SHARED_PROFILE_HOME/bash/env
+source $SHARED_PROFILE_HOME/bash/aliases
+source $SHARED_PROFILE_HOME/bash/functions
