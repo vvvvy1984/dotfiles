0a7121d29a88fadfc4131cd3c6ef44d33ea220b8
Cosmin Stejerean <cstejerean@gmail.com>
2008-11-26 15:10:04 -0600

    trying to make tab indent or expand

--- setup.el
+++ setup.el
@@ -117,3 +117,24 @@
 (add-hook 'python-mode-hook 'show-ws-highlight-trailing-whitespace)
 (define-key ctl-x-map "t" 'untabify)
 (setq-default indent-tabs-mode nil)
+
+;; tab indent or expand
+
+(defun indent-or-expand (arg)
+  "Either indent according to mode, or expand the word preceding
+point."
+  (interactive "*P")
+  (if (and
+       (or (bobp) (= ?w (char-syntax (char-before))))
+       (or (eobp) (not (= ?w (char-syntax (char-after))))))
+      (dabbrev-expand arg)
+    (indent-according-to-mode)))
+ 
+(defun my-tab-fix ()
+  (local-set-key [tab] 'indent-or-expand))
+ 
+;; add hooks for modes you want to use the tab completion for:
+(add-hook 'python-mode-hook          'my-tab-fix)
+(add-hook 'sh-mode-hook         'my-tab-fix)
+(add-hook 'emacs-lisp-mode-hook 'my-tab-fix)
+(add-hook 'clojure-mode-hook    'my-tab-fix)
