948cf2aec15c149b989124d06c406eb7da4dae4e
Cosmin Stejerean <cosmin@offbytwo.com>
2009-03-20 21:56:14 -0500

    updating externals will now only re-compile clojure if there are changes to clojure or clojure-contrib and if asked to do so

--- update-externals.sh
+++ update-externals.sh
@@ -1,18 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+CLOJURE_CHANGES=0
+if [ "$1" = "--clj" ]; then
+    RECOMPILE_CLOJURE_ON_CHANGE='true'
+else
+    RECOMPILE_CLOJURE_ON_CHANGE=''
+fi
 
 pull_master() {
+    echo "`pwd`"
     git checkout master
-    git pull origin
+    git pull origin | grep 'Already up-to-date.'
 }
 
 cd src/clojure
-pull_master
-ant clean jar
+pull_master || CLOJURE_CHANGES=1
 cd ../..
 
 cd src/clojure-contrib
-pull_master
-ant clean jar
+pull_master || CLOJURE_CHANGES=1
 cd ../..
 
 cd emacs-lisp/slime
@@ -26,3 +32,11 @@ cd ../..
 cd emacs-lisp/clojure-mode
 pull_master
 cd ../..
+
+cd gitext
+./update-externals.sh
+cd ..
+
+if [[ $CLOJURE_CHANGES > 0 && $RECOMPILE_CLOJURE_ON_CHANGE = 'true' ]]; then
+    ./compile-clojure.sh
+fi
