8c95be698df46315dcc42789c58eab1a30958974
Cosmin Stejerean <cosmin@offbytwo.com>
2010-09-09 22:27:48 -0500

    Changed install.sh to match the new installation procedure for bash and emacs

--- install.sh
+++ install.sh
@@ -5,16 +5,13 @@ INSTALL_LOCATION="`pwd`"
 
 if [[ $OS = "Darwin" ]]; then
     BASHRC="~/.profile"
-    defaults write ~/.MacOSX/environment SHARED_PROFILE_HOME $INSTALL_LOCATION
-    echo "Added SHARED_PROFILE_HOME to ~/.MacOSX/environment"
+    #defaults write ~/.MacOSX/environment SHARED_PROFILE_HOME $INSTALL_LOCATION
 else
     BASHRC="~/.bashrc"
-    echo "export SHARED_PROFILE_HOME=$INSTALL_LOCATION" >> $BASHRC
-    echo "Added SHARED_PROFILE_HOME to ~/.bashrc"
 fi
 
-echo "source $INSTALL_LOCATION/setup.sh" >> $BASHRC
-echo "Added Shared Profile setup.sh to Bash"
+echo "source $INSTALL_LOCATION/bash/init.sh" >> $BASHRC
+echo "Added dotfiles init.sh to Bash"
 
-echo '(load (concat (getenv "SHARED_PROFILE_HOME") "/" "setup.el"))' >> ~/.emacs
-echo "Added Shared Profile setup.el to Emacs"
+ln -sf $INSTALL_LOCATION/emacs-lisp ~/.emacs.d
+echo "Symlinked ~/.emacs.d to emacs-lisp"
