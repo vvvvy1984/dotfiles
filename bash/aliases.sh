f649a23c673276d9707000993935b3f9df2b5b4b
Cosmin Stejerean <cosmin@offbytwo.com>
2010-11-14 20:20:29 -0600

    fixed ls to work with Darwin

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,11 @@
-alias ls='ls --color=auto'
+OS=`uname`
+
+if [[ $OS == "Darwin" ]]; then
+    alias ls='ls -G'
+else
+    alias ls='ls --color=auto'
+fi
+
 alias ll="ls -lh"
 alias la="ls -Ah"
 alias e="emacs -nw"
